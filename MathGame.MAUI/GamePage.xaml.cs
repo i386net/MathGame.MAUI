bfea6237a63b50d7925c95ef4c8da332bb23232c
DK <i386.net@gmail.com>
2022-11-03 11:30:57 +0300

    Add logic for submitting answer Add ProcessAnswer method Add GameOver method

--- MathGame.MAUI/GamePage.xaml.cs
+++ MathGame.MAUI/GamePage.xaml.cs
@@ -5,6 +5,9 @@ public partial class GamePage : ContentPage
 	public string GameType { get; set; }
 	private int firstNumber = 0;
 	private int secondNumber = 0;
+	private int score = 0;
+	private const int totalQuestions = 3;
+	private int questionsLeft = totalQuestions;
 	public GamePage(string gameType)
 	{
 		InitializeComponent();
@@ -18,7 +21,7 @@ public partial class GamePage : ContentPage
 		var gameOperand = GameType switch
 		{
 			"Addition" => "+",
-            "Subtraction" => "-",
+			"Subtraction" => "-",
 			"Multiplication" => "*",
 			"Division" => "/",
 			_ => "",
@@ -26,6 +29,7 @@ public partial class GamePage : ContentPage
 		var random = new Random();
 		firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
 		secondNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+		AnswerEntry.Text = ""; // clear text field
 
 		if (GameType == "Division")
 		{
@@ -39,6 +43,51 @@ public partial class GamePage : ContentPage
 	}
 	private void OnAnswerSubmitted(object sender, EventArgs e)
 	{
+		//int answer = int.Parse(AnswerEntry.Text); // check for incorrect input
+		int.TryParse(AnswerEntry.Text, out int answerNumber);
+		bool isCorrect = false;
 
+		switch (GameType)
+		{
+			case "Addition":
+				isCorrect = answerNumber == firstNumber + secondNumber;
+				break;
+			case "Subtraction":
+				isCorrect = answerNumber == firstNumber - secondNumber;
+				break;
+			case "Multiplication":
+				isCorrect = answerNumber == firstNumber * secondNumber;
+				break;
+			case "Division":
+				isCorrect = answerNumber == firstNumber / secondNumber;
+				break;
+			default:
+				break;
+		}
+		ProcessAnswer(isCorrect);
+		questionsLeft -= 1;
+		AnswerLabel.Text = ""; // clear label 
+		if (questionsLeft > 0)
+		{
+			CreateNewQuestion();
+		}
+		else
+		{
+			GameoVer();
+		}
+	}
+
+	private void GameoVer()
+	{
+		GameOverLabel.Text = $"Game over! Your got {score} out of {totalQuestions}";
+	}
+
+	private void ProcessAnswer(bool isCorrect)
+	{
+		if (isCorrect)
+		{
+			score += 1;
+		}
+		AnswerLabel.Text = isCorrect ? "Correct!" : "Incorrect";
 	}
 }
