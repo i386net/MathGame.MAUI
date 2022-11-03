6d47e5a159801f478e82a26f227751de054fce36
DK <i386.net@gmail.com>
2022-11-03 10:14:46 +0300

    Add game logic to generate question and submite the answer

--- MathGame.MAUI/GamePage.xaml.cs
+++ MathGame.MAUI/GamePage.xaml.cs
@@ -3,10 +3,42 @@ namespace MathGame.MAUI;
 public partial class GamePage : ContentPage
 {
 	public string GameType { get; set; }
+	private int firstNumber = 0;
+	private int secondNumber = 0;
 	public GamePage(string gameType)
 	{
 		InitializeComponent();
 		GameType = gameType;
 		BindingContext = this;
+		CreateNewQuestion();
+	}
+
+	private void CreateNewQuestion()
+	{
+		var gameOperand = GameType switch
+		{
+			"Addition" => "+",
+            "Subtraction" => "-",
+			"Multiplication" => "*",
+			"Division" => "/",
+			_ => "",
+		};
+		var random = new Random();
+		firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+		secondNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+
+		if (GameType == "Division")
+		{
+			while (firstNumber < secondNumber || firstNumber % secondNumber != 0)
+			{
+				firstNumber = random.Next(1, 99);
+				secondNumber = random.Next(1, 99);
+			}
+		}
+		QuestionLabel.Text = $"{firstNumber} {gameOperand} {secondNumber}";
+	}
+	private void OnAnswerSubmitted(object sender, EventArgs e)
+	{
+
 	}
 }
