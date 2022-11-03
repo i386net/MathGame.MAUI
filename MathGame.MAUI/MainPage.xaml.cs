131ad525ac52551ebbca38f4b5a9371a23c47cd2
DK <i386.net@gmail.com>
2022-11-03 07:59:56 +0300

    Add OnGameChosen method

--- MathGame.MAUI/MainPage.xaml.cs
+++ MathGame.MAUI/MainPage.xaml.cs
@@ -6,5 +6,11 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnGameChosen(object sender, EventArgs e)
+	{
+		Button button = (Button)sender; //cast sender to Button
+		Navigation.PushAsync(new GamePage(button.Text));
+	}
 }
 
