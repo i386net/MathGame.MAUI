888e27ca3dbcbb4a86e6a0c1277d63316c11aab3
DK <i386.net@gmail.com>
2022-11-03 08:40:08 +0300

    Previous Games logic

--- MathGame.MAUI/MainPage.xaml.cs
+++ MathGame.MAUI/MainPage.xaml.cs
@@ -9,11 +9,15 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnGameChosen(object sender, EventArgs e)
-	{
-		Button button = (Button)sender; //cast sender to Button
-		Debug.WriteLine(button.Text);
-		Navigation.PushAsync(new GamePage(button.Text));
-	}
+    private void OnGameChosen(object sender, EventArgs e)
+    {
+        Button button = (Button)sender; //cast sender to Button
+        Debug.WriteLine(button.Text);
+        Navigation.PushAsync(new GamePage(button.Text));
+    }
+    private void OnViewPrevousGamesChosen(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new PreviousGames());
+    }
 }
 
