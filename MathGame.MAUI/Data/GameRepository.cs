7d3dd44f215d7f47c7fbc3bdf1b50a65a8e3f52f
DK <i386.net@gmail.com>
2022-11-05 12:01:54 +0300

    Added data to GameRepository
    Added fields 
    Added Init method

--- MathGame.MAUI/Data/GameRepository.cs
+++ MathGame.MAUI/Data/GameRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace MathGame.MAUI.Data
+﻿using MathGame.MAUI.Models;
+using SQLite;
+
+namespace MathGame.MAUI.Data
 {
-    internal class GameRepository
+    public class GameRepository
     {
+        private string _dbPath;
+        private SQLiteConnection conn;
+
+        public GameRepository(string dbPath)
+        {
+            _dbPath = dbPath;   
+
+        }
+
+        public void Init()
+        {
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Game>();
+        }
     }
 }
