75c6ba22601f123648e39680e429312d7614598b
DK <i386.net@gmail.com>
2022-11-05 11:34:12 +0300

    Added props and enum
    Added props to Game class
    Added enum for game types

--- MathGame.MAUI/Models/Game.cs
+++ MathGame.MAUI/Models/Game.cs
@@ -2,5 +2,14 @@
 {
     public class Game
     {
+        public int Id { get; set; }
+        public GameType Type { get; set; }
+        public DateTime DatePlayed { get; set; }
+        public int Score { get; set; }
+    }
+
+    public enum GameType
+    {
+        Addition, Subtraction, Multiplication, Division
     }
 }
